78115925500c75929b01d2cc06d2e1796886d1bf
Himanshu-thg <Himanshu.Mukhedkar43@thg.com>
2025-01-01 13:09:52 +0530

    fix: config issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from "@astrojs/vercel"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
-
   build: {
     outDir: "dist", // Ensure this matches your deployment folder
   },
