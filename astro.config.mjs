98bb1d5c4f04a5564a7a26b48714d6af44a39fdb
Himanshu-thg <Himanshu.Mukhedkar43@thg.com>
2025-01-01 12:57:13 +0530

    added vercel adapter and move images to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import vercel from "@astrojs/vercel"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  build: {
-    outDir: "dist", // Ensure this matches your deployment folder
-  },
+  output: 'server',
+  adapter: vercel(),
+
+  // build: {
+  //   outDir: "dist", // Ensure this matches your deployment folder
+  // },
   trailingSlash: "never", // Or "always" depending on your preference
 });
